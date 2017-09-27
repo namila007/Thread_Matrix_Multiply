7f12a889a367308cfa5c9a8db55e543bf8e3064b
Namila Bandara <namila007@gmail.com>
2017-09-28 02:41:37 +0530

    commented the files.

--- src/Core/ControlMatrix.java
+++ src/Core/ControlMatrix.java
@@ -25,28 +25,18 @@ public class ControlMatrix  {
         int i=0;
         int Alength=0;
         Alength=A.length/threadcount;//size of the rows
-
-
         threads=new Thread[threadcount];
-
         for(i=0;i<threadcount-1;i++){
             //starting objects
             threads[i]=new Thread(new Threading(A,B,finalmatrix,new int[]{i*Alength,(i+1)*Alength}));
         }
         threads[i]=new Thread(new Threading(A,B,finalmatrix,new int[]{i*Alength,A.length}));
         try {
-            for(Thread k:threads){
-                k.start();
-            }
-            for(Thread k:threads){
-               k.join();
-          }
+            for(Thread k:threads)k.start();
+            for(Thread k:threads) k.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-
-
     }
 
     public void writeMatrixData(BufferedWriter bw) throws IOException{
@@ -59,7 +49,6 @@ public class ControlMatrix  {
                 bw.write(String.format("%.4f",finalmatrix.getMatrix()[m][n]));
                 bw.newLine();
             }
-
         }
 
     }
